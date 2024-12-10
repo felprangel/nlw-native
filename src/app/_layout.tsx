397fe8d3214ebeec907295236d222655685e3f06
felprangel <feliperangel897@gmail.com>
2024-12-10 07:29:20 -0300

    utiliza as fontes

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,20 @@
 import { colors } from "@/styles/theme";
+import {
+  Rubik_400Regular,
+  Rubik_500Medium,
+  Rubik_600SemiBold,
+  Rubik_700Bold,
+  useFonts,
+} from "@expo-google-fonts/rubik";
 import { Stack } from "expo-router";
 
 export default function Layout() {
+  useFonts({
+    Rubik_400Regular,
+    Rubik_500Medium,
+    Rubik_600SemiBold,
+    Rubik_700Bold,
+  });
   return (
     <Stack
       screenOptions={{
