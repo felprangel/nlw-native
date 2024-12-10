a644f160448482b3dd6bd5c61fa3a47edbb4f33d
felprangel <feliperangel897@gmail.com>
2024-12-10 20:59:31 -0300

    adiciona estilização e componente de boas vindas

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,10 @@
+import { Welcome } from "@/components/Welcome";
 import { Text, View } from "react-native";
 
 export default function Home() {
   return (
-    <View>
-      <Text>Hello world!</Text>
+    <View style={{ flex: 1, padding: 40, gap: 40 }}>
+      <Welcome />
     </View>
   );
 }
