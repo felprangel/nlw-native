fd0016a0c608cd03ca361ea499eed3e8d8685042
felprangel <feliperangel897@gmail.com>
2024-12-10 21:13:59 -0300

    utiliza componente de steps

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,3 +1,4 @@
+import { Steps } from "@/components/Steps";
 import { Welcome } from "@/components/Welcome";
 import { Text, View } from "react-native";
 
@@ -5,6 +6,7 @@ export default function Home() {
   return (
     <View style={{ flex: 1, padding: 40, gap: 40 }}>
       <Welcome />
+      <Steps />
     </View>
   );
 }
