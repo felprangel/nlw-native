faa5839a00340977939f525d935ff9533f3e228d
felprangel <feliperangel897@gmail.com>
2024-12-10 21:31:53 -0300

    utiliza step com o texto certo

--- src/components/Steps/index.tsx
+++ src/components/Steps/index.tsx
@@ -6,7 +6,18 @@ export function Steps() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Veja como funciona:</Text>
-      <Step />
+      <Step
+        title="Encontre estabelecimentos"
+        description="Veja locais perto de você que são parceiros Nearby"
+      />
+      <Step
+        title="Ative o cupom com QR Code"
+        description="Escaneie o código no estabelecimento para usar o benefício"
+      />
+      <Step
+        title="Garanta vantagens perto de você"
+        description="Ative cupons onde estiver, em diferentes tipos de estabelecimento "
+      />
     </View>
   );
 }
