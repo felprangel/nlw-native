cbca25c802475dbc6acdc476a0664ff3a5c952fc
felprangel <feliperangel897@gmail.com>
2024-12-11 06:59:29 -0300

    adiciona componente de ícone

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -7,9 +7,14 @@ import {
 } from "react-native";
 import { styles } from "./styles";
 import { colors } from "@/styles/colors";
+import { IconProps as TablerIconProps } from "@tabler/icons-react-native";
 
 type ButtonProps = TouchableOpacityProps & {
   isLoading?: boolean;
+};
+
+type IconProps = {
+  icon: React.ComponentType<TablerIconProps>;
 };
 
 function Button(props: ButtonProps) {
@@ -33,6 +38,11 @@ function Title(props: TextProps) {
   return <Text style={styles.title}>{props.children}</Text>;
 }
 
+function Icon(props: IconProps) {
+  return <props.icon size={24} color={colors.gray[100]} />;
+}
+
 Button.Title = Title;
+Button.Icon = Icon;
 
 export { Button };
