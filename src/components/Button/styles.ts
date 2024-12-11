6e73dfbf896bbe7ff766f29ad511840975fb89c2
felprangel <feliperangel897@gmail.com>
2024-12-10 22:14:05 -0300

    ajusta estilização

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,6 +1,21 @@
+import { colors } from "@/styles/colors";
+import { fontFamily } from "@/styles/font-family";
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-  container: {},
-  title: {},
+  container: {
+    height: 56,
+    maxHeight: 56,
+    backgroundColor: colors.green.base,
+    borderRadius: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row",
+    gap: 14,
+  },
+  title: {
+    color: colors.gray[100],
+    fontFamily: fontFamily.semiBold,
+    fontSize: 16,
+  },
 });
