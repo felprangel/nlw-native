d76789c6ccb762ae37b8c15f1cfff922df888709
felprangel <feliperangel897@gmail.com>
2024-12-10 21:13:08 -0300

    adiciona componente de steps

---
+++ src/components/Steps/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import { styles } from "./styles";
+
+export function Steps() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Veja como funciona:</Text>
+    </View>
+  );
+}
