645b2a8ea89e6d0ae81d49d91ae4dbb230f6928c
felprangel <feliperangel897@gmail.com>
2024-12-10 21:14:07 -0300

    remove import

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,6 @@
 import { Steps } from "@/components/Steps";
 import { Welcome } from "@/components/Welcome";
-import { Text, View } from "react-native";
+import { View } from "react-native";
 
 export default function Home() {
   return (
