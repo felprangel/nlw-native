52563391e1bbc826dfed56458840e2ffb018f728
felprangel <feliperangel897@gmail.com>
2024-12-10 21:25:01 -0300

    utiliza componente step

--- src/components/Steps/index.tsx
+++ src/components/Steps/index.tsx
@@ -1,10 +1,12 @@
 import { Text, View } from "react-native";
 import { styles } from "./styles";
+import { Step } from "../Step";
 
 export function Steps() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Veja como funciona:</Text>
+      <Step />
     </View>
   );
 }
