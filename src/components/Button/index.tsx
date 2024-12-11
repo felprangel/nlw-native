599a2aa60168eca6392ce3176494f628ee90c850
felprangel <feliperangel897@gmail.com>
2024-12-10 22:14:14 -0300

    utiliza estilizações

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -4,15 +4,18 @@ import {
   TouchableOpacity,
   TouchableOpacityProps,
 } from "react-native";
+import { styles } from "./styles";
 
 function Button(props: TouchableOpacityProps) {
   return (
-    <TouchableOpacity activeOpacity={0.8}>{props.children}</TouchableOpacity>
+    <TouchableOpacity activeOpacity={0.8} style={styles.container}>
+      {props.children}
+    </TouchableOpacity>
   );
 }
 
 function Title(props: TextProps) {
-  return <Text>{props.children}</Text>;
+  return <Text style={styles.title}>{props.children}</Text>;
 }
 
 Button.Title = Title;
