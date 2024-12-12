af6e6a8f8adbbbd3f38a9418cf6796e7d67bf0d2
felprangel <feliperangel897@gmail.com>
2024-12-11 21:24:17 -0300

    chama a função de buscar as categorias

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -1,7 +1,12 @@
 import { api } from "@/services/api";
+import { useEffect } from "react";
 import { Alert, Text, View } from "react-native";
 
 export default function Home() {
+  useEffect(() => {
+    fetchCategories();
+  });
+
   async function fetchCategories() {
     try {
       const { data } = await api.get("/categories");
