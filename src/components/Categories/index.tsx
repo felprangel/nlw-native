4b5eeebee0c34caf31a5b7195545e3c72164c710
felprangel <feliperangel897@gmail.com>
2024-12-11 21:35:13 -0300

    utiliza componente de categoria

--- src/components/Categories/index.tsx
+++ src/components/Categories/index.tsx
@@ -1,5 +1,10 @@
 import { View } from "react-native";
+import { Category } from "../Category";
 
 export function Categories() {
-  return <View></View>;
+  return (
+    <View>
+      <Category></Category>
+    </View>
+  );
 }
