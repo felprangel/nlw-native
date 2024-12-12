249585174cec26fbca8eebd212e3032c8c9bc1e9
felprangel <feliperangel897@gmail.com>
2024-12-12 20:52:38 -0300

    adiciona state

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -3,9 +3,12 @@ import { api } from "@/services/api";
 import { useEffect, useState } from "react";
 import { Alert, View } from "react-native";
 
+type MarketsProps = PlaceProps & {};
+
 export default function Home() {
   const [categories, setCategories] = useState<CategoriesProps[]>([]);
   const [category, setCategory] = useState("");
+  const [markets, setMarkets] = useState<MarketsProps[]>([]);
 
   useEffect(() => {
     fetchCategories();
@@ -26,6 +29,7 @@ export default function Home() {
     try {
       if (!category) return;
       const { data } = await api.get(`/markets/category/${category}`);
+      setMarkets(data);
     } catch (error) {
       console.error(error);
       Alert.alert("Locais", "Não foi possível carregar os locais");
