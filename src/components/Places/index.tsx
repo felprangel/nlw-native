ec9de17879d4ca3370b0bad768c4930a8db3cfc0
felprangel <feliperangel897@gmail.com>
2024-12-13 07:13:49 -0300

    adiciona estilizações

--- src/components/Places/index.tsx
+++ src/components/Places/index.tsx
@@ -1,7 +1,8 @@
-import { useWindowDimensions } from "react-native";
+import { Text, useWindowDimensions } from "react-native";
 import { Place, PlaceProps } from "../Place";
 import BottomSheet, { BottomSheetFlatList } from "@gorhom/bottom-sheet";
 import { useRef } from "react";
+import { styles } from "./styles";
 
 type Props = {
   data: PlaceProps[];
@@ -19,11 +20,19 @@ export function Places(props: Props) {
     <BottomSheet
       ref={bottomSheetRef}
       snapPoints={[snapPoint.min, snapPoint.max]}
+      handleIndicatorStyle={styles.indicator}
+      backgroundStyle={styles.container}
+      enableOverDrag={false}
     >
       <BottomSheetFlatList
         data={props.data}
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => <Place data={item} />}
+        contentContainerStyle={styles.content}
+        ListHeaderComponent={() => (
+          <Text style={styles.title}>Explore locais perto de você</Text>
+        )}
+        showsVerticalScrollIndicator={false}
       />
     </BottomSheet>
   );
