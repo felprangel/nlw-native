47479858f09118fad82724c82019e775b24ed428
felprangel <feliperangel897@gmail.com>
2024-12-13 07:04:09 -0300

    adiciona bottom sheet

--- src/components/Places/index.tsx
+++ src/components/Places/index.tsx
@@ -1,10 +1,22 @@
-import { View } from "react-native";
-import { PlaceProps } from "../Place";
+import { useWindowDimensions } from "react-native";
+import { Place, PlaceProps } from "../Place";
+import BottomSheet, { BottomSheetFlatList } from "@gorhom/bottom-sheet";
+import { useRef } from "react";
 
 type Props = {
   data: PlaceProps[];
 };
 
 export function Places(props: Props) {
-  return <View />;
+  const dimensions = useWindowDimensions();
+  const bottomSheetRef = useRef<BottomSheet>(null);
+  return (
+    <BottomSheet>
+      <BottomSheetFlatList
+        data={props.data}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <Place data={item} />}
+      />
+    </BottomSheet>
+  );
 }
