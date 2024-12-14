0bdbd5bf5b3a41ca38d93fcba13adb200f4077a5
felprangel <feliperangel897@gmail.com>
2024-12-14 20:08:46 -0300

    adicoina estilização

---
+++ src/components/Details/styles.ts
@@ -0,0 +1,10 @@
+import { DevSettings, StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {},
+  name: {},
+  description: {},
+  group: {},
+  title: {},
+  rule: {},
+});
