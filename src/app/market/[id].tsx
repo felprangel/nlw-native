63372d177e4c1d87231074621126ede7d13b3d7a
felprangel <feliperangel897@gmail.com>
2024-12-14 19:33:28 -0300

    adiciona função para buscar os dados

--- src/app/market/[id].tsx
+++ src/app/market/[id].tsx
@@ -1,8 +1,21 @@
-import { useLocalSearchParams } from "expo-router";
-import { Text, View } from "react-native";
+import { api } from "@/services/api";
+import { router, useLocalSearchParams } from "expo-router";
+import { Alert, Text, View } from "react-native";
 
 export default function Market() {
   const params = useLocalSearchParams<{ id: string }>();
+
+  async function fetchMarket() {
+    try {
+      const { data } = await api.get(`/markets/${params.id}`);
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+      Alert.alert("Erro", "Não foi possível carregar os dados", [
+        { text: "OK", onPress: () => router.back() },
+      ]);
+    }
+  }
 
   return (
     <View style={{ flex: 1 }}>
