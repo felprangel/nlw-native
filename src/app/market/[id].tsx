5589d8a324ccc6ccdaf5673187a6de9649facf58
felprangel <feliperangel897@gmail.com>
2024-12-14 19:52:58 -0300

    adicoina chamada de função

--- src/app/market/[id].tsx
+++ src/app/market/[id].tsx
@@ -1,14 +1,17 @@
 import { api } from "@/services/api";
 import { router, useLocalSearchParams } from "expo-router";
+import { useEffect } from "react";
 import { Alert, Text, View } from "react-native";
 
 export default function Market() {
   const params = useLocalSearchParams<{ id: string }>();
+  useEffect(() => {
+    fetchMarket();
+  }, [params]);
 
   async function fetchMarket() {
     try {
       const { data } = await api.get(`/markets/${params.id}`);
-      console.log(data);
     } catch (error) {
       console.error(error);
       Alert.alert("Erro", "Não foi possível carregar os dados", [
